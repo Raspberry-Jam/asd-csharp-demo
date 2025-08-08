e8ea8ec19de75f8252e3d31d1ba7674f06cc8e90
James Chivers <raspberryjam.chivers@gmail.com>
2025-08-08 18:17:04 +1000

    Set up database connection

--- ScheduleServer/Program.cs
+++ ScheduleServer/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using ScheduleServer.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,6 +8,10 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<IScheduleDbContext, ScheduleDbContext>(options => 
+    options.UseSqlite(connectionString));
 
 var app = builder.Build();
 
